6b5de02bce417a46f92f209252f445b5eeeba2b5
Kevin Rowe <kevfro@gmail.com>
2022-01-12 21:12:25 -0800

    Changes cron job to run every hour rather than every minute

--- scripts/add_cron_job.sh
+++ scripts/add_cron_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 crontab -l > crontab_new 
-echo "* * * * * /home/pi/Documents/hockeysched/scripts/hockeysched.sh > /home/pi/Documents/hockeysched/logs/hockeysched.txt" >> crontab_new
+echo "0 * * * * /home/pi/Documents/hockeysched/scripts/hockeysched.sh > /home/pi/Documents/hockeysched/logs/hockeysched.txt" >> crontab_new
 echo "" >> crontab_new
 crontab crontab_new
 rm crontab_new
